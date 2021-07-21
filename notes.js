e086759c0e4d4b34a937770849114c32d62d4467
Johnathon Brown <73731974+DopeEBear@users.noreply.github.com>
2021-07-21 16:45:15 -0700

    Add files via upload
    This is a command line notes app made from Node.js only

---
+++ notes.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+// Add New Note
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNote = notes.find((note) => note.title === title);
+
+  if (!duplicateNote) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log(chalk.green.inverse('New note added!'));
+  } else {
+    console.log(chalk.red.inverse('Note title taken!'));
+  }
+};
+
+// Remove Note
+const removeNote = (title) => {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter((note) => note.title !== title);
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse('Note removed!'));
+  } else {
+    console.log(chalk.red.inverse('Note not found!'));
+  }
+
+  saveNotes(notesToKeep);
+};
+
+// List Notes
+const listNotes = () => {
+  const notes = loadNotes();
+
+  console.log(chalk.blue.inverse('Your Notes'));
+
+  notes.forEach((note) => {
+    console.log(chalk.blue(note.title));
+  });
+};
+
+// read Note
+const readNote = (title) => {
+  const notes = loadNotes();
+  const note = notes.find((note) => note.title === title);
+
+  if (note) {
+    console.log(chalk.inverse(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.red.inverse('Note not found!'));
+  }
+};
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = {
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote,
+};
